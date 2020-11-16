c1d7c1abf39e72108ad381ab20bf25398f57c37b
Andrii-Vekker <vekker304@gmail.com>
2020-11-16 23:06:25 +0200

    wrgtwreg

---
+++ js/task-4.js
@@ -0,0 +1,5 @@
+const ref = {
+    incrementRef: document.querySelector('button [data-action = "increment"]')
+    decrementRef: document.querySelector('button [data-action = "decrement"]')
+    valueRef: document.querySelector('#value')
+}
