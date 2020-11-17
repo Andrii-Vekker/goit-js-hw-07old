5d1c55edffefb950ba94e826ffd89bdf55479377
Andrii-Vekker <vekker304@gmail.com>
2020-11-17 14:21:01 +0200

    res

--- js/task-5.js
+++ js/task-5.js
@@ -1,18 +1,18 @@
-// const inputRef = document.querySelector('#name-input');
-// const nameRef = document.querySelector('#name-output')
-// inputRef.addEventListener('input', handleInputChange)
+const inputRef = document.querySelector('#name-input');
+const nameRef = document.querySelector('#name-output')
+inputRef.addEventListener('input', handleInputChange)
 
-// function handleInputChange(event) {
+function handleInputChange(event) {
 
-//     inputRef.value  ? nameRef.textContent = event.target.value : nameRef.textContent = `незнакомец`;
-// }
-// const ref = {
-//     inputRef: document.querySelector('#name-input'),
-//     spanRef: document.querySelector('#name-output')
-// };
-// ref.inputRef.addEventListener('input', handleTextInput);
-// function handleTextInput(event) {
-//     ref.inputRef.value
-//         ? ref.spanRef.textContent = event.target.value
-//         : ref.spanRef.textContent = 'незнакомец';
-// };
+    inputRef.value  ? nameRef.textContent = event.target.value : nameRef.textContent = `незнакомец`;
+}
+const ref = {
+    inputRef: document.querySelector('#name-input'),
+    spanRef: document.querySelector('#name-output')
+};
+ref.inputRef.addEventListener('input', handleTextInput);
+function handleTextInput(event) {
+    ref.inputRef.value
+        ? ref.spanRef.textContent = event.target.value
+        : ref.spanRef.textContent = 'незнакомец';
+};
